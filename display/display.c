2c86bbc84401c7b5266695d9d9821ae85c5e1172
ArchMage <archmage@mylittlepwnies.net>
2011-01-31 19:59:31 -0800

    first commit

---
+++ display/display.c
@@ -0,0 +1,97 @@
+//*****************************************************************************
+//
+// display.c - Simple example to display the on-board LCD.
+//
+// Copyright (c) 2009-2011 Texas Instruments Incorporated.  All rights reserved.
+// Software License Agreement
+// 
+// Texas Instruments (TI) is supplying this software for use solely and
+// exclusively on TI's microcontroller products. The software is owned by
+// TI and/or its suppliers, and is protected under applicable copyright
+// laws. You may not combine this software with "viral" open-source
+// software in order to form a larger program.
+// 
+// THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
+// NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
+// NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
+// CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
+// DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 6852 of the EK-LM3S9B92 Firmware Package.
+//
+//*****************************************************************************
+
+#include "inc/lm3s9b92.h"
+#include "inc/hw_types.h"
+#include "driverlib/debug.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "drivers/display96x16x1.h"
+//*****************************************************************************
+//
+//! \addtogroup example_list
+//! <h1>Blinky (blinky)</h1>
+//!
+//! A very simple example that blinks the on-board LED.
+//
+//*****************************************************************************
+
+//*****************************************************************************
+//
+// Blink the on-board LED.
+//
+//*****************************************************************************
+int main(void)
+{
+    volatile unsigned long ulLoop;
+
+    //
+    // Enable the GPIO port that is used for the on-board LED.
+    //
+    SYSCTL_RCGC2_R = SYSCTL_RCGC2_GPIOF;
+
+    //
+    // Do a dummy read to insert a few cycles after enabling the peripheral.
+    //
+    ulLoop = SYSCTL_RCGC2_R;
+
+    //
+    // Enable the GPIO pin for the LED (PD0).  Set the direction as output, and
+    // enable the GPIO pin for digital function.
+    //
+    GPIO_PORTF_DIR_R |= 0x10;
+    GPIO_PORTF_DEN_R |= 0x10;
+
+    //
+    // Loop forever.
+    //
+    Display96x16x1Init(false);
+    Display96x16x1StringDraw("ArchMage!!...", 6, 0);
+    while(1)
+    {
+        //
+        // Turn on the LED.
+        //
+        GPIO_PORTF_DATA_R |= 0x10;
+
+        //
+        // Delay for a bit.
+        //
+        for(ulLoop = 0; ulLoop < 200000; ulLoop++)
+        {
+        }
+
+        //
+        // Turn off the LED.
+        //
+        GPIO_PORTF_DATA_R &= ~(0x10);
+
+        //
+        // Delay for a bit.
+        //
+        for(ulLoop = 0; ulLoop < 200000; ulLoop++)
+        {
+        }
+    }
+}
